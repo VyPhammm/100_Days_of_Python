07dfd494f6b7acec0a2acaadcb8d7f7cbfd4f9b9
Vy Pham <127418764+VyPhammm@users.noreply.github.com>
2023-04-04 00:01:32 +0700

    Update scoreboard.py

--- Day20_Snake-Game/scoreboard.py
+++ Day20_Snake-Game/scoreboard.py
@@ -13,9 +13,15 @@ class Scoreboard(Turtle):
         self.write(f"Your Score: {self.score}", align= ALIGNMENT, font= FONT)
         self.hideturtle()
     
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over! Your final Score: {self.score}", align= ALIGNMENT, font= FONT)
+    
     def plus_score(self):
         self.clear()
         self.score +=1
         self.write(f"Your Score: {self.score}", align= "center", font=("Arial", 15, "normal"))
+
+
 
 
