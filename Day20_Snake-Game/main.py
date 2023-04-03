c020b477d2d1a85d36d7d6ea56706702b446dcdb
Vy Pham <vy.phgia@gmail.com>
2023-04-03 23:03:52 +0700

    snakegame

--- Day20_Snake-Game/main.py
+++ Day20_Snake-Game/main.py
@@ -1,6 +1,8 @@
-from turtle import Turtle, Screen
-import time
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
 
 
 screen = Screen()
@@ -11,6 +13,8 @@ screen.title("My snake game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -25,6 +29,10 @@ while game_is_on :
     time.sleep(0.1)
     snake.move()
 
+    #Detect collision with food
+    if snake.head.distance(food) < 15 :
+        food.refresh()
+        scoreboard.plus_score()
 
 
 
