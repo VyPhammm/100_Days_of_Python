68c021da0439bd7aa4a780d0efe892b7ec2d32e8
Vy Pham <vy.phgia@gmail.com>
2023-04-09 22:46:11 +0700

    update

---
+++ Day29_Password-Manager/main.py
@@ -0,0 +1,83 @@
+from tkinter import *
+from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    password_letters= [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols= [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers= [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.delete(0, END)
+    password_entry.insert(0, string= password)
+    pyperclip.copy(password)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_data():
+    website = website_entry.get()
+    user = user_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(user) == 0 or len(password) == 0 :
+        messagebox.showinfo(title= "Oops", message= "Please don't leave any fields empty")
+    else:
+        is_oke = messagebox.askokcancel(title= website, message= f"These are details entered: \n Username: {user} \n Password: {password} \nIs it ok to save? ")
+        if is_oke:
+            with open(r"D:\100_Days_of_Python\Day29_Password-Manager\data.txt", "a") as data :
+                data.write(f"{website} | {user} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx= 50, pady= 50)
+
+#Logo
+canvas = Canvas(width= 200, height= 200)
+logo_image = PhotoImage(file= r"D:\100_Days_of_Python\Day29_Password-Manager\logo.png")
+canvas.create_image(100, 100, image= logo_image)
+canvas.grid(column= 1, row= 0)
+
+#Row 1:
+website_label = Label(text="Website: ")
+website_label.grid(column= 0, row= 1)
+
+website_entry = Entry(width=52, highlightthickness= 0)
+website_entry.grid(column= 1, row= 1, columnspan=2, sticky=W)
+website_entry.focus()
+
+#Row 2:
+user_label = Label(text="Email/Username: ")
+user_label.grid(column= 0, row= 2)
+
+user_entry = Entry(width=52, highlightthickness= 0)
+user_entry.grid(column= 1, row= 2, columnspan=2, sticky=W)
+user_entry.insert(0, "phamvy@gmail.com")
+
+#Row 3:
+password_label = Label(text="Password: ")
+password_label.grid(column= 0, row= 3)
+
+password_entry = Entry(width=30)
+password_entry.insert(END, string="")
+password_entry.grid(column= 1, row= 3, sticky=W)
+
+generate_button = Button(text="Generate Password", command= password_generator)
+generate_button.grid(column= 2, row= 3, sticky=E)
+
+#Row 4:
+add_button = Button(text="Add", width=44, highlightthickness= 0, command= save_data)
+add_button.grid(column= 1, row= 4, columnspan= 2, sticky=W)
+
+
+window.mainloop()
